dd0ab08fe8f53bfaa68953f62b60a74031e5c932
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-25 10:32:45 -0300

    DepartmentList controller

---
+++ src/main/java/com/example/javafxmysqlproject/controllers/DepartmentListController.java
@@ -0,0 +1,50 @@
+package com.example.javafxmysqlproject.controllers;
+
+import com.example.javafxmysqlproject.Main;
+import com.example.javafxmysqlproject.model.entities.Department;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentListController implements Initializable {
+
+    @FXML
+    private Button newButton;
+
+    @FXML
+    private void onNewButtonAction() {
+        System.out.println("New Action");
+    }
+
+    @FXML
+    private TableView<Department> tableViewDepartment;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+
+}
---
+++ src/main/java/com/example/javafxmysqlproject/model/entities/Department.java
@@ -0,0 +1,54 @@
+package com.example.javafxmysqlproject.model.entities;
+
+import java.util.Objects;
+
+public class Department {
+
+    private Integer id;
+    private String name;
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Department() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
